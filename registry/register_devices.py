1cad650ae90191e778eb88173094a315577a5a5f
Arman Khatamsaz <armankhatamsaz1378@gmail.com>
2026-03-04 23:24:37 +0100

    update register devices outdoor sensors

--- registry/register_devices.py
+++ registry/register_devices.py
@@ -2,11 +2,19 @@ import requests
 
 URL = 'http://127.0.0.1:8080/devices'
 
-# لیست دستگاه‌هایی که طبق پروپوزال باید در سیستم ثبت بشن
+# لیست کامل دستگاه‌ها (داخل، بیرون و اکچویتور)
 devices = [
+    # سنسورهای داخل (Indoor)
     {"device_id": "temp_in_01", "type": "Temperature", "location": "Indoor", "mqtt_topic": "home/sensor/temp/indoor"},
     {"device_id": "hum_in_01", "type": "Humidity", "location": "Indoor", "mqtt_topic": "home/sensor/hum/indoor"},
     {"device_id": "light_in_01", "type": "Light", "location": "Indoor", "mqtt_topic": "home/sensor/light/indoor"},
+    
+    # سنسورهای بیرون (Outdoor) - جدید
+    {"device_id": "temp_out_01", "type": "Temperature", "location": "Outdoor", "mqtt_topic": "home/sensor/temp/outdoor"},
+    {"device_id": "hum_out_01", "type": "Humidity", "location": "Outdoor", "mqtt_topic": "home/sensor/hum/outdoor"},
+    {"device_id": "light_out_01", "type": "Light", "location": "Outdoor", "mqtt_topic": "home/sensor/light/outdoor"},
+    
+    # عملگرها (Actuator)
     {"device_id": "window_actuator_01", "type": "Actuator", "location": "Living Room", "mqtt_topic": "home/actuator/window"}
 ]
 
