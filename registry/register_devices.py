912af6f46bb2a41b2ddf20450ee8bd10ae20008c
Arman Khatamsaz <armankhatamsaz1378@gmail.com>
2026-03-10 01:18:35 +0100

    update registery_device for dockerization

--- registry/register_devices.py
+++ registry/register_devices.py
@@ -1,20 +1,20 @@
 import requests
+import time
 
-URL = 'http://127.0.0.1:8080/devices'
+# صبر می‌کنیم تا سرور رجیستری تو داکر کاملاً بالا بیاد
+time.sleep(3)
 
-# لیست کامل دستگاه‌ها (داخل، بیرون و اکچویتور)
+# به جای 127.0.0.1 از اسم سرویس در داکر (registry) استفاده می‌کنیم
+URL = 'http://registry:8080/devices'
+
+# لیست کامل دستگاه‌ها
 devices = [
-    # سنسورهای داخل (Indoor)
     {"device_id": "temp_in_01", "type": "Temperature", "location": "Indoor", "mqtt_topic": "home/sensor/temp/indoor"},
     {"device_id": "hum_in_01", "type": "Humidity", "location": "Indoor", "mqtt_topic": "home/sensor/hum/indoor"},
     {"device_id": "light_in_01", "type": "Light", "location": "Indoor", "mqtt_topic": "home/sensor/light/indoor"},
-    
-    # سنسورهای بیرون (Outdoor) - جدید
     {"device_id": "temp_out_01", "type": "Temperature", "location": "Outdoor", "mqtt_topic": "home/sensor/temp/outdoor"},
     {"device_id": "hum_out_01", "type": "Humidity", "location": "Outdoor", "mqtt_topic": "home/sensor/hum/outdoor"},
     {"device_id": "light_out_01", "type": "Light", "location": "Outdoor", "mqtt_topic": "home/sensor/light/outdoor"},
-    
-    # عملگرها (Actuator)
     {"device_id": "window_actuator_01", "type": "Actuator", "location": "Living Room", "mqtt_topic": "home/actuator/window"}
 ]
 
